aeea7d51d9f4b2c51961438c80bfcec3050559f6
John Rhay <javier.johnrhay@gmail.com>
2025-05-09 00:26:14 +0800

    feat: add scroll to top component

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -2,11 +2,13 @@ import Footer from "@/components/Footer"
 import Header from "@/components/header/Header"
 import MiniHero from "@/components/MiniHero"
 import Navbar from "@/components/navbar/Navbar"
+import ScrollToTop from "@/components/ScrollToTop"
 import { Outlet } from "react-router"
 
 const RootLayout = () => {
     return (
         <div className="flex flex-col min-h-screen">
+            <ScrollToTop />
             <Header />
             <Navbar />
             <MiniHero />
